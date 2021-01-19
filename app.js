23617dde4fd4b7b75531bc862742bd0c60375db8
Brandon O <b.ondayko@gmail.com>
2021-01-19 14:46:37 -0600

    First 3 Lessons

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = Vue.createApp({
     data() {     //Shorthand to make a function
         return { //be sure to return an object
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            url:"https://www.google.com",
+            books: [
+                {title: 'name of the wind', author: 'Patrick Ruffus', img: './imgs/placeholder2.jpg', isFav: true},
+                {title: 'The Hobbit', author: 'Tolkein', img: './imgs/placeholder2.jpg', isFav: true},
+                {title: 'Harry Potter', author: 'The Lady', img: './imgs/placeholder3.jpg', isFav: false}
+            ]
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // x: 0,
+            // y: 0
         }
     },
     //Functions that can be called
@@ -15,6 +22,25 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data) {
+                console.log(data);
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        },
+        isFavChange(book) {
+            console.log(book);
+            book.isFav = !book.isFav;
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter( (book) => book.isFav)
         }
     }
 })
